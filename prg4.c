97d638853fafc541c1e87f36484537f3327dbafe
priyankakatgeri <160142340+priyankakatgeri@users.noreply.github.com>
2025-09-08 15:42:23 +0530

    lab4.c

--- prg4.c
+++ prg4.c
@@ -1,1 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    int rank, size,number;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size < 2) {
+        if (rank == 0) {
+            printf("This program requires at least 2 processes.\n");
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
+    if (rank == 0) {
+        number = 42; // Example data
+        printf("Process 0 sending number %d to process 1\n", number);
+        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    } else if (rank == 1) {
+        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received number %d from process 0\n", number);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
